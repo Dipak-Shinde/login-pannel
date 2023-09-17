08634eb70dd9ebac7b1023ffa052247b13040ec7
Dipak Shinde <dipakshinde@gmail.com>
2023-09-17 15:51:45 +0530

    first commit

---
+++ loginsystem/includes/footer.php
@@ -0,0 +1,13 @@
+   <div id="layoutAuthentication_footer">
+                <footer class="py-4 bg-light mt-auto">
+                    <div class="container-fluid px-4">
+                        <div class="d-flex align-items-center justify-content-between small">
+                            <div class="text-muted">Copyright &copy; <?php echo date('Y');?></div>
+                            <div>
+                                <a href="https://dipak-shinde.github.io/Website/ " target="_blank">Website | DipakShinde</a>
+    
+                            </div>
+                        </div>
+                    </div>
+                </footer>
+            </div>
